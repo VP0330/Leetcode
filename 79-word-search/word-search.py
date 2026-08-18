c3e853add9793138ad3a6b8ad8523fe7d5950f69
VP0330 <vaibhav.parmarms@gmail.com>
2026-08-18 17:33:37 -0400

    Time: 3393 ms (77.52%) | Memory: 19.5 MB (57.91%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -3,13 +3,13 @@ class Solution:
         rows=len(board)
         cols=len(board[0])
         def dfs(r,c,i):
-            if i==len(word):
+            if i == len(word):
                 return True
             if (r<0 or r>=rows or c<0 or c>=cols or board[r][c]!=word[i]):
                 return False
             temp=board[r][c]
             board[r][c]="#"
-            found=(dfs(r+1,c,i+1)  or
+            found=(dfs(r+1,c,i+1)  or 
                     dfs(r-1,c,i+1) or
                     dfs(r,c+1,i+1) or
                     dfs(r,c-1,i+1))
